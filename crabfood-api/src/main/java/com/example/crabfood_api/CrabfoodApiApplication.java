4c85d328a76b409175b5a0a93df9c753d69ae877
ntlam25 <lam2k3d@gmail.com>
2025-04-12 11:07:47 +0700

    initial project

---
+++ crabfood-api/src/main/java/com/example/crabfood_api/CrabfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crabfood_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabfoodApiApplication.class, args);
+	}
+
+}
---
+++ crabfood-api/src/test/java/com/example/crabfood_api/CrabfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crabfood_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
